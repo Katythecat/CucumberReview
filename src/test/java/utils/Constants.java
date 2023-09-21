9197616679a9a5c48bc881a224f8b8844aaaf1ad
Katythecat <kk.wilasinee@gmail.com>
2023-09-21 00:01:48 -0700

    cry

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -3,7 +3,7 @@ package utils;
 public class Constants {
     public static int WAIT_TIME=1000;
     public static String PROPERTY_FILE_PATH=
-            System.getProperty("user.dir")+"/src/test/resources/config/Config.properties";
+            System.getProperty("user.dir")+"/src/test/resources/Config/Config.properties";
     public static final String TESTDATA_FILEPATH =
             System.getProperty("user.dir") + "/src/test/resources/testdata/batch14excel.xlsx";
     public static final String SCREENSHOT_FILEPATH =System.getProperty("user.dir")+"/screenshots/";
